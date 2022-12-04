bfd0c2551211e03633c66222fb87a19301f623bf
JosephSantos742 <josephsantos742@gmail.com>
2022-12-03 23:53:57 -0300

    Adicionado informações sobre a loja

--- eCommerce/wwwroot/js/site.js
+++ eCommerce/wwwroot/js/site.js
@@ -1,4 +1,20 @@
-﻿// Configuração do Modal
+﻿// Funções para trocar os textos com as informações da loja
+const textoSobre = document.getElementById('textoSobre').innerHTML;
+const textoValores = document.getElementById('textoValores').innerHTML;
+const textoEpi = document.getElementById('textoEPI').innerHTML;
+
+function trocarTextoSobre() {
+    document.getElementById('textoSobre').innerHTML = textoSobre;
+}
+function trocarTextoValores() {
+    document.getElementById('textoSobre').innerHTML = textoValores;
+}
+function trocarTextoEPI() {
+    document.getElementById('textoSobre').innerHTML = textoEpi;
+}
+
+
+// Configuração do Modal de login 
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
