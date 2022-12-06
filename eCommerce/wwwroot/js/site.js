01083da3c2043833ce554e107720aa7ec22ce320
JosephSantos742 <josephsantos742@gmail.com>
2022-12-06 00:08:09 -0300

    Adicionado modal de cadastro com API via CEP, responsividade alguns itens e botão voltar ao topo

--- eCommerce/wwwroot/js/site.js
+++ eCommerce/wwwroot/js/site.js
@@ -15,16 +15,76 @@ function trocarTextoEPI() {
 
 
 // Configuração do Modal de login 
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const openModalLogin = document.querySelector("#open-modal-login");
+const closeModalLogin = document.querySelector("#close-modal-login");
+const modalLogin = document.querySelector("#modal-login");
+const fadeLogin = document.querySelector("#fade-login");
 
-const toggleModal = () => {
-    modal.classList.toggle("hide");
-    fade.classList.toggle("hide");
+const toggleModalLogin = () => {
+    modalLogin.classList.toggle("hide");
+    fadeLogin.classList.toggle("hide");
 };
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
+[openModalLogin, closeModalLogin, fadeLogin].forEach((el) => {
+    el.addEventListener("click", () => toggleModalLogin());
 });
+
+// Configuração do Modal de cadastro
+const openModalCadastro = document.querySelector("#open-modal-cadastro");
+const closeModalCadastro = document.querySelector("#close-modal-cadastro");
+const modalCadastro = document.querySelector("#modal-cadastro");
+const fadeCadastro = document.querySelector("#fade-cadastro");
+
+const toggleModalCadastro = () => {
+    modalCadastro.classList.toggle("hide");
+    fadeCadastro.classList.toggle("hide");
+};
+
+[openModalCadastro, closeModalCadastro, fadeCadastro].forEach((el) => {
+    el.addEventListener("click", () => toggleModalCadastro());
+});
+
+// Consumindo API do ViaCep para preencher campos de endereço automático
+
+    // Função para limpar os campos do formulário
+const limparFormulario = (endereco) => {
+    document.getElementById('logradouro').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('complemento').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+}
+
+    // Função para preencher os campos caso o cep seja válido
+const preencherFormulario = (endereco) => {
+    document.getElementById('logradouro').value = endereco.logradouro;
+    document.getElementById('complemento').value = endereco.complemento;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+}
+
+    // Expressões regulares para permitir somente numeros de 0 a 9
+const eNumero = (numero) => /^[0-9]+$/
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisaCep = async () => {
+    limparFormulario();
+    const cep = document.getElementById('cep').value;
+    // Passando um cep para a api do VIACEP, caso for valido vai retornar um json com os dados
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('logradouro').value = 'CEP não encontrado'
+        } else {
+            preencherFormulario(endereco)
+        }
+    } else {
+        document.getElementById('logradouro').value = 'CEP incorreto'
+    }
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisaCep);
